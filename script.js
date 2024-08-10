84f5a1547929e5e5dccca87a4d988aae10045e19
Haseeb <chhaseebamjad0@gmail.com>
2024-08-10 15:20:06 +0500

    Fully complete Hero Section

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 let navToggle = document.getElementsByClassName("nav-toggle")[0];
 let navLinks = document.getElementsByClassName("nav-links")[0];
+let heroContent = document.getElementsByClassName('hero-content')[0];
 
 navToggle.addEventListener("click", () => {
     navLinks.classList.toggle("active");
     navToggle.classList.toggle("active");
+
+    if (navLinks.classList.contains('active')) {
+        heroContent.classList.add('dimmed');
+    } else {
+        heroContent.classList.remove('dimmed');
+    }
 })
